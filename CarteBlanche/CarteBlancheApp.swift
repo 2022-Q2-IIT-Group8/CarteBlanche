b32e4c90c10323a28486229732abba3450213778
Sky Velleity <velleity@outlook.com.au>
2022-07-28 10:08:43 +1000

    Initial Commit

---
+++ CarteBlanche/CarteBlancheApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarteBlancheApp.swift
+//  CarteBlanche
+//
+//  Created by Sky on 2022/7/28.
+//
+
+import SwiftUI
+
+@main
+struct CarteBlancheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
